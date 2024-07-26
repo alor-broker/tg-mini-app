b55678d844509db6e04cc8a12514ed192da7b165
sbelashevskiy <sbelashevskiy@gmail.com>
2024-07-26 15:09:40 +0500

    Home screen. Portfolio positions

---
+++ projects/tg-app/src/app/home/components/positions-list/positions-list.component.ts
@@ -0,0 +1,77 @@
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import { SelectedPortfolioDataContextService } from "../../services/selected-portfolio-data-context.service";
+import {
+  PortfolioPosition,
+  PortfolioPositionsService
+} from "@api-lib";
+import {
+  Observable,
+  shareReplay,
+  switchMap
+} from "rxjs";
+import { map } from "rxjs/operators";
+import {
+  AsyncPipe,
+  DecimalPipe
+} from "@angular/common";
+import { NzTypographyComponent } from "ng-zorro-antd/typography";
+import { NzAvatarComponent } from "ng-zorro-antd/avatar";
+import { InstrumentIconSourceService } from "../../../core/services/instrument-icon-source.service";
+
+@Component({
+  selector: 'tga-positions-list',
+  standalone: true,
+  imports: [
+    AsyncPipe,
+    DecimalPipe,
+    NzTypographyComponent,
+    NzAvatarComponent
+  ],
+  templateUrl: './positions-list.component.html',
+  styleUrl: './positions-list.component.less'
+})
+export class PositionsListComponent implements OnInit {
+  positions$!: Observable<PortfolioPosition[]>;
+
+  constructor(
+    private readonly selectedPortfolioDataContextService: SelectedPortfolioDataContextService,
+    private readonly apiPortfolioPositionsService: PortfolioPositionsService,
+    private readonly instrumentIconSourceService: InstrumentIconSourceService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.positions$ = this.selectedPortfolioDataContextService.selectedPortfolio$.pipe(
+      switchMap(p => this.apiPortfolioPositionsService.getAllForPortfolio(p.portfolioKey)),
+      map(p => p ?? []),
+      shareReplay(1)
+    );
+  }
+
+  showLots(position: PortfolioPosition): boolean {
+    return position.symbol !== 'RUB'
+  }
+
+  positionTitle(position: PortfolioPosition): string {
+    if (position.symbol !== 'RUB') {
+      return position.shortName;
+    }
+
+    return 'Рубли'
+  }
+
+  iconSymbol(position: PortfolioPosition): string{
+    if (position.symbol !== 'RUB') {
+      return position.symbol;
+    }
+
+    return '₽'
+  }
+
+  getIconUrl(position: PortfolioPosition): string {
+    return this.instrumentIconSourceService.getIconUrl(position.symbol);
+  }
+}
--- projects/tg-app/src/app/home/pages/home-page/home-page.component.ts
+++ projects/tg-app/src/app/home/pages/home-page/home-page.component.ts
@@ -5,6 +5,7 @@ import {
 } from "ng-zorro-antd/collapse";
 import { PortfolioSelectionComponent } from "../../components/portfolio-selection/portfolio-selection.component";
 import { PortfolioEvaluationComponent } from "../../components/portfolio-evaluation/portfolio-evaluation.component";
+import { PositionsListComponent } from "../../components/positions-list/positions-list.component";
 
 @Component({
   selector: 'tga-home-page',
@@ -13,7 +14,8 @@ import { PortfolioEvaluationComponent } from "../../components/portfolio-evaluat
     NzCollapseComponent,
     NzCollapsePanelComponent,
     PortfolioSelectionComponent,
-    PortfolioEvaluationComponent
+    PortfolioEvaluationComponent,
+    PositionsListComponent
   ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.less'
