2e711ee59e92d546370651d08150a08034c5baff
Daniil Markov <agushasok@mail.ru>
2024-08-07 15:23:01 +0300

    add prod config

---
+++ projects/tg-app/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  apiUrl: 'https://api.alor.ru',
+  userDataUrl: 'https://lk-api.alor.ru',
+  ssoUrl: 'https://login.alor.ru',
+  superAppUrl: 'https://tgminiapp.alor.ru/superapp',
+
+  externalLinks: {
+    astras: "https://astras.alor.ru",
+    personalAccount: "https://lk.alor.ru/",
+    tgAlor: "https://t.me/alorbroker"
+  }
+};
