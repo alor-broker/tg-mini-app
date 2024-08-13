45d68c85a31142b3c723aad382f67cdd5e89352d
Daniil Markov <agushasok@mail.ru>
2024-08-13 16:24:05 +0300

    loader fix

--- projects/tg-app/src/app/core/components/page-loader/page-loader.component.ts
+++ projects/tg-app/src/app/core/components/page-loader/page-loader.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, OnChanges } from '@angular/core';
 import { NzIconDirective } from "ng-zorro-antd/icon";
 
 @Component({
@@ -10,6 +10,15 @@ import { NzIconDirective } from "ng-zorro-antd/icon";
   templateUrl: './page-loader.component.html',
   styleUrl: './page-loader.component.less'
 })
-export class PageLoaderComponent {
+export class PageLoaderComponent implements OnChanges {
   @Input({ required: true }) isVisible!: boolean;
+
+  constructor(
+    private readonly cdr: ChangeDetectorRef
+  ) {
+  }
+
+  ngOnChanges() {
+    this.cdr.detectChanges();
+  }
 }
